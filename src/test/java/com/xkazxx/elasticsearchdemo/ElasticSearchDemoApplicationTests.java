1b2e1ffdf6c6a20dab102677ce08d5d3223d87c1
xkazxx <xkazxx@qq.com>
2021-12-21 16:36:48 +0800

    init

---
+++ src/test/java/com/xkazxx/elasticsearchdemo/ElasticSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkazxx.elasticsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
